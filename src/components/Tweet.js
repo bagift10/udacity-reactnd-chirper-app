d1b3f446179b11047c886df281f88763631721c1
Brandon Gift <bagift10@gmail.com>
2021-06-17 23:11:57 -0400

    feat: Navigation with react-router-dom

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { formatTweet, formatDate } from '../utils/helpers';
 import { TiArrowBackOutline, TiHeartOutline, TiHeartFullOutline } from 'react-icons/ti';
 import { handleToggleTweet } from '../actions/tweets';
+import { Link, withRouter } from 'react-router-dom';
 
 class Tweet extends Component {
   handleLike = (e) => {
@@ -18,7 +19,7 @@ class Tweet extends Component {
 
   toParent =(e, id) => {
     e.preventDefault();
-    
+    this.props.history.push(`/tweet/${id}`)
   }
 
   render() {
@@ -36,11 +37,12 @@ class Tweet extends Component {
       hasLiked,
       likes,
       replies,
+      id,
       parent
     } = tweet;
 
     return(
-      <div className='tweet'>
+      <Link to={`/tweet/${id}`} className='tweet'>
         <img
           src={avatar}
           alt={`Avatar of ${name}`}
@@ -76,7 +78,7 @@ class Tweet extends Component {
             <span>{likes !== 0 && likes}</span>
           </div>
         </div>
-      </div>
+      </Link>
     )
   }
 }
@@ -91,4 +93,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
   }
 }
 
-export default connect(mapStateToProps)(Tweet);
+export default withRouter(connect(mapStateToProps)(Tweet));
